b04b669d3dd3004b6ce3e2888f991e82e2436a72
aleciaortiz923-star <alecia.ortiz923@gmail.com>
2026-04-22 13:34:17 -0400

    firebase

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,16 +1,27 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyAyUnjSnnLplk1iXHXXn4C8npx8YHAvrV0",
+  authDomain: "summarise-3419f.firebaseapp.com",
+  databaseURL: "https://summarise-3419f-default-rtdb.firebaseio.com",
+  projectId: "summarise-3419f",
+  storageBucket: "summarise-3419f.appspot.com",
+  messagingSenderId: "739852733376",
+  appId: "1:739852733376:web:a975966bb8ff8c21db1625",
+  measurementId: "G-W1REQLEK4Z"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const storage = getStorage(app);
+const analytics = getAnalytics(app);
+const db = getFirestore(app);
 
-export { auth };
+export { auth, storage, analytics, db };
